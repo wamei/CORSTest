420d9447198f3f0def165dcb711157a5ff14437c
wamei <wamei.cho@gmail.com>
2019-12-02 00:31:55 +0900

    update server

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ cd Server
 node test.js
 ```
 
-[http://localhost:4000/index.html](http://localhost:4000/index.html) でブラウザで確認もできます
+[http://localhost:4000](http://localhost:4000/index.html) でブラウザで確認もできます
--- Server/test.js
+++ Server/test.js
@@ -5,31 +5,47 @@ const server = http.createServer();
 const origin = 'http://127.0.0.1:8080';
 
 server.on('request', function(req, res) {
+    console.log(`url = ${req.url}`);
     console.log(`origin = ${req.headers.origin}`);
-    if (req.url == '/index.html') {
-        fs.readFile(__dirname + '/../Resources/index.html', 'utf-8', function (err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type' : 'text/plain'});
-                res.write('page not found');
-                return res.end();
-            }
 
-            res.writeHead(200, {'Content-Type' : 'text/html'});
-            res.write(data);
-            res.end();
-        });
-        return;
+    if (req.url == '/') {
+        req.url = '/index.html';
     }
-    const headers = {
-        'Content-Type' : 'text/plain',
-    };
-    if (req.headers.origin == origin) {
-        headers['Access-Control-Allow-Origin'] = origin;
-        headers['Access-Control-Allow-Credentials'] = 'true';
-    }
-    res.writeHead(200, headers);
-    res.write('ok');
-    res.end();
+
+    const headers = {};
+    fs.readFile(`${__dirname}/../Resources${req.url}`, 'utf-8', function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type' : 'text/plain'});
+            res.write('not found');
+            return res.end();
+        }
+
+        if (req.url.endsWith(".ico")) {
+            headers['Content-Type'] = "image/x-icon";
+        } else if (req.url.endsWith(".png") || req.url.endsWith(".PNG")) {
+            headers['Content-Type'] = "image/png";
+        } else if (req.url.endsWith(".jpg") || req.url.endsWith(".JPG") || req.url.endsWith(".jpeg") || req.url.endsWith(".JPEG")) {
+            headers['Content-Type'] = "image/jpeg";
+        } else if (req.url.endsWith(".js")) {
+            headers['Content-Type'] = "application/javascript";
+        } else if (req.url.endsWith(".css")) {
+            headers['Content-Type'] = "text/css";
+        } else if (req.url.endsWith(".html") || req.url.endsWith(".htm")) {
+            headers['Content-Type'] = "text/html";
+        } else if (req.url.endsWith(".map")) {
+            headers['Content-Type'] = "application/json";
+        } else {
+            headers['Content-Type'] = "text/plain";
+        }
+        if (req.headers.origin == origin) {
+            headers['Access-Control-Allow-Origin'] = origin;
+            headers['Access-Control-Allow-Credentials'] = 'true';
+        }
+
+        res.writeHead(200, headers);
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(4000);
